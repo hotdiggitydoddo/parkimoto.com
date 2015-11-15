71cdaf1946a3324abbb180f0ac2c8f4c0bb53061
hotdiggitydoddo <hotdiggitydoddo@gmail.com>
2015-11-15 02:48:16 -0500

    set env vars

--- parkimoto-backend/config/env/production.js
+++ parkimoto-backend/config/env/production.js
@@ -25,7 +25,7 @@ module.exports = {
    * Set the port in the production environment to 80                        *
    ***************************************************************************/
 
-  // port: 80,
+  port: 5005,
 
   /***************************************************************************
    * Set the log level in production environment to "silent"                 *
@@ -34,14 +34,6 @@ module.exports = {
   // log: {
   //   level: "silent"
   // }
-
-    service: 'Gmail',
-    auth: {
-      user: 'parkimotoevents@gmail.com',
-      pass: 'superfun'
-    },
-    from: 'no-reply@test.com',
-    testMode: false
   
 
 };
